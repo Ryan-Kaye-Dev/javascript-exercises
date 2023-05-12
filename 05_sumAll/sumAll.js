b0e44f4be0e7cbccedb61506eae9cf5d836b663b
clappedidiot <115187240+clappedidiot@users.noreply.github.com>
2023-05-12 22:12:07 +0100

    complete task 6

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,19 +1,18 @@
 const sumAll = function (a, b) {
-    if ( a < 0 || b < 0) {
-        return "ERROR"
-    }
+  if (a < 0 || b < 0) {
+    return "ERROR";
+  }
 
-    if (typeof(a) !== 'number' || typeof(b) !== 'number' ) {
-        return "ERROR"
-    }
-
+  if (typeof a !== "number" || typeof b !== "number") {
+    return "ERROR";
+  }
 
   let sum = 0;
 
   for (let i = Math.min(...arguments); i <= Math.max(...arguments); i++) {
     sum += i;
   }
-  return sum; 
+  return sum;
 };
 
 // Do not edit below this line
