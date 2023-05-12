ac1c843a8b226304a8e4540a6f9ed85aeeeb1907
clappedidiot <115187240+clappedidiot@users.noreply.github.com>
2023-05-12 21:22:38 +0100

    complete up to exercise 4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,9 @@
-const removeFromArray = function() {
+let removeFromArray = (arr, ...args) => {
+    return arr.filter((item) => {
+        return !args.includes(item)
+    })
+}
 
-};
-
+console.log(removeFromArray([1,2,3], 1, 2));
 // Do not edit below this line
 module.exports = removeFromArray;
