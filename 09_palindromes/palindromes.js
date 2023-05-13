f7617b05555f0563f4551a3c822efd7a608ba8f1
clappedidiot <115187240+clappedidiot@users.noreply.github.com>
2023-05-13 20:45:27 +0100

    Complete Task 09

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,7 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+    let splitString = string.toLowerCase().replace(/[^a-z]/g,"").split("");
+    return splitString.join("") == splitString.reverse().join("");
 };
-
+console.log(palindromes("ZZZZ car, a man, a maracaz."));
 // Do not edit below this line
 module.exports = palindromes;
