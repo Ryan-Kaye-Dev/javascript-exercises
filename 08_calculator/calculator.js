73a33cd2e00b2a43987e7b31bdd962b63605e520
clappedidiot <115187240+clappedidiot@users.noreply.github.com>
2023-05-13 19:51:41 +0100

    Complete task 08

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,43 @@
-const add = function() {
-	
+const add = function(a,b) {
+	return a + b
 };
 
-const subtract = function() {
-	
+const subtract = function(a,b) {
+	return a - b
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+  let sum = 0;
+	for (let i = 0; i < arr.length; i++){
+    sum += arr[i]
+  }
+  return sum
 };
 
-const multiply = function() {
+const multiply = function(arr) {
+  let multiplied = arr[0];
+  for (let i = 1; i < arr.length; i++){
+    multiplied *= arr[i]
+  }
+  return multiplied
+}
 
+const power = function(num, power) {
+  return Math.pow(num,power)
 };
 
-const power = function() {
-	
-};
 
-const factorial = function() {
-	
+
+const factorial = function(num) {
+  if (num == 0 || num == 1){
+    return 1
+  } else {
+    let factorial = 1;
+    for (let i = 1; i <= num; i++){
+      factorial *= i
+    }
+    return factorial
+  }
 };
 
 // Do not edit below this line
